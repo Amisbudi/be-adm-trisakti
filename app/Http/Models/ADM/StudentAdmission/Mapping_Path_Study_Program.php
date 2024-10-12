f0593d3454aac302f1204bbaf041fb642f964a70
haisyamm <haisyammaulana22@gmail.com>
2024-10-12 23:40:37 +0700

    Fix Calculate

--- app/Http/Models/ADM/StudentAdmission/Mapping_Path_Study_Program.php
+++ app/Http/Models/ADM/StudentAdmission/Mapping_Path_Study_Program.php
@@ -55,17 +55,22 @@ class Mapping_Path_Study_Program extends Model
             'msp.active_status',
             'msp.minimum_donation',
             'msp.id',
-            'sum(mapping_path_price.price) as price',
             DB::raw('case when msp.active_status =' . "'t'" . ' then ' . "'Aktif'" . ' else ' . "'Non Aktif'" . ' end as active_status_name'),
-            DB::raw("CASE WHEN msp.is_technic = 't' THEN msp.is_technic ELSE 'false' END AS is_technic")
+            DB::raw("CASE WHEN msp.is_technic = 't' THEN msp.is_technic ELSE 'false' END AS is_technic"),
+            // Add a new column to calculate the sum of prices
+            DB::raw('SUM(mpp.price) AS total_price') // Replace 'mpp.price' with the actual price column name
         )
-            ->join('study_programs as t1', 'msp.program_study_id', '=', 't1.classification_id')
-            ->leftJoin('mapping_path_price as mpp', 'msp.program_study_id', '=', 'mpp.study_program_id')
-            ->where('msp.selection_path_id', '=', $selection_path_id)
-            ->where([$active_status, $filter_id, $is_technic_filter])
-            ->distinct()
-            ->get();
-
+        ->join('study_programs as t1', 'msp.program_study_id', '=', 't1.classification_id')
+        ->leftJoin('mapping_path_price AS mpp', function ($join) use ($selection_path_id) {
+            $join->on('msp.program_study_id', '=', 'mpp.study_program_id');
+            // Optional: Add filtering conditions on 'mpp' table if needed
+            // $join->where('mpp.some_condition', '=', $some_value);
+        })
+        ->where('msp.selection_path_id', '=', $selection_path_id)
+        ->where([$active_status, $filter_id, $is_technic_filter])
+        ->groupBy('msp.selection_path_id', 't1.study_program_branding_name', 't1.classification_id', 't1.faculty_id', 't1.faculty_name', 'msp.quota', 'msp.active_status', 'msp.minimum_donation', 'msp.id', 'active_status_name', 'is_technic') // Group by relevant columns
+        ->get();
+        
         return $data;
     }
 }
